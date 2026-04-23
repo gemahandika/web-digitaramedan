7b79451beebfaee1bee8f1f0b63cdbda791247a8
Gema Handika <ggemahandika88@gmail.com>
2026-04-23 16:45:48 +0700

    update

--- app/init.php
+++ app/init.php
@@ -3,4 +3,4 @@ require_once 'config/config.php';
 require_once 'core/Controller.php';
 require_once 'core/Database.php';
 require_once 'core/Flasher.php';
-require_once __DIR__ . '/../vendor/autoload.php';
+// require_once __DIR__ . '/../vendor/autoload.php';
